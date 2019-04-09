6d306503937d130adb0c3f89024f9d72f43a46a4
bruasu <bruasu@gmail.com>
2019-04-08 23:00:53 -0300

    update 8-04-19

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexModule } from './home/index/index.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexModule
   ],
   imports: [
     BrowserModule,
