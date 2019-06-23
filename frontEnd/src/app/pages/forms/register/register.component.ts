f2fd6aff82f782e5f84161ea9a7216f1ef803ab4
bruasu <bruasu@gmail.com>
2019-06-23 12:20:02 -0300

    finish verification form in Angular

--- frontEnd/src/app/pages/forms/register/register.component.ts
+++ frontEnd/src/app/pages/forms/register/register.component.ts
@@ -22,9 +22,19 @@ export class RegisterComponent implements OnInit {
   @ViewChild('Dlogin') Dlogin;
   @ViewChild('DpasswordAlert') DpasswordAlert;
   @ViewChild('DrepasswordAlert') DrepasswordAlert;
+  @ViewChild('dataInvalid') DdataInvalid;
 
   statusSearchUserLogin: boolean;
+  statusSearchUserEmail: boolean;
+
   statusAlertUserLogin: number = 0;
+  statusAlertUserEmail: number = 0;
+
+  verificationEmail: boolean = false;
+  verificationLogin: boolean = false;
+  verificationPassword: boolean = false;
+  verificationName: boolean = false;
+
 
   constructor(
     private Suser: UserService,
@@ -36,21 +46,44 @@ export class RegisterComponent implements OnInit {
     //console.log(this.Dpassword);
   }
 
-  registerUser(){
-    let date = {
-      name: this.name,
-      login: this.login,
-      email: this.email,
-      password: this.password,
-      repassword: this.repassword,
-      type_user: this.type_user
-    }
-    if(this.password == this.repassword){
-      this.Suser.registerUser(date);      
+  registerUser(){    
+
+    if(this.verificationEmail && this.verificationLogin && this.verificationName && this.verificationPassword){
+      
+      let date = {
+        name: this.name,
+        login: this.login,
+        email: this.email,
+        password: this.password,
+        repassword: this.repassword,
+        type_user: this.type_user
+      }
+      
+      this.Suser.registerUser(date).subscribe((res)=>{
+        console.log(res);
+      },(err)=>{
+        console.log(err);
+        this.render.removeClass(this.DdataInvalid.nativeElement, 'd-none');
+      });
+        
+      
     }else{
-
+      this.render.removeClass(this.DdataInvalid.nativeElement, 'd-none');
     }
 
+  }
+  checkName(){
+    if(this.name && this.name.length > 0){
+
+      this.verificationName = false;
+      
+      if(this.name.length > 0){
+        this.verificationName = true;
+        this.render.addClass(this.Dname.nativeElement, 'is-valid');
+      }else{
+        this.render.removeClass(this.Dname.nativeElement, 'is-valid');
+      }
+    }
   }
   checkPassword(){
     setTimeout(() => {
@@ -64,7 +97,8 @@ export class RegisterComponent implements OnInit {
       }
       
       // check if they are equal       
-      if(this.password == this.repassword){    
+      if(this.password == this.repassword){   
+        this.verificationPassword = true;
         this.render.addClass(this.DrepasswordAlert.nativeElement, 'd-none');
         
         this.render.removeClass(this.Dpassword.nativeElement, 'is-invalid');
@@ -73,6 +107,7 @@ export class RegisterComponent implements OnInit {
         this.render.addClass(this.Drepassword.nativeElement, 'is-valid');
         
       }else if(this.password != this.repassword && this.repassword != null){
+        this.verificationPassword = false;
         this.render.removeClass(this.Dpassword.nativeElement, 'is-valid');
         this.render.removeClass(this.Drepassword.nativeElement, 'is-valid');
         this.render.addClass(this.Dpassword.nativeElement, "is-invalid");
@@ -81,6 +116,7 @@ export class RegisterComponent implements OnInit {
         this.render.removeClass(this.DrepasswordAlert.nativeElement, 'd-none');
       }
       if(this.password == '' && this.repassword == ''){
+        this.verificationPassword = false;
         this.render.addClass(this.DrepasswordAlert.nativeElement, 'd-none');
 
         this.render.removeClass(this.Dpassword.nativeElement, 'is-valid');
@@ -91,26 +127,30 @@ export class RegisterComponent implements OnInit {
 
     }, 500);
   }
-  checkLoginUserSearch(){    
-    //request server
-    if(this.login.length > 5){
-      this.statusSearchUserLogin = true;
+  checkLoginUserSearch(){
+    if(this.login && this.login.length > 0){
 
+      //request server
+      if(this.login.length > 5){
+        this.statusSearchUserLogin = true;
+        this.verificationLogin = false;
+        
       setTimeout(()=>{
-        console.log('get');
         if(this.statusSearchUserLogin){
           this.statusSearchUserLogin = false;
-          console.log('get request');
+          console.log('get request Login');
           this.Suser.checkLoginUser(this.login).subscribe((res) => {         
             
             if(res.msj){
+              this.verificationLogin = false;
               this.statusAlertUserLogin = 2;
-            this.render.removeClass(this.Dlogin.nativeElement, 'is-valid');
-            this.render.addClass(this.Dlogin.nativeElement, 'is-invalid');          
-          }
+              this.render.removeClass(this.Dlogin.nativeElement, 'is-valid');
+              this.render.addClass(this.Dlogin.nativeElement, 'is-invalid');          
+            }
         },(err) => {
           if(err.status == 400){
             if(err.error.msj == false){
+              this.verificationLogin = true;
               this.statusAlertUserLogin = 0;  
               this.render.removeClass(this.Dlogin.nativeElement, 'is-invalid');
               this.render.addClass(this.Dlogin.nativeElement, 'is-valid');                
@@ -124,9 +164,64 @@ export class RegisterComponent implements OnInit {
     if(this.login.length > 5){
       this.statusAlertUserLogin = 0;
     }else{
-      this.statusAlertUserLogin = 1;
-      this.render.removeClass(this.Dlogin.nativeElement, 'is-valid');
-      this.render.removeClass(this.Dlogin.nativeElement, 'is-invalid');
+      if(this.login.length > 0){
+        this.statusAlertUserLogin = 1;
+      }else{
+        this.verificationLogin = false;
+        this.statusAlertUserLogin = 0
+        this.render.removeClass(this.Dlogin.nativeElement, 'is-valid');
+        this.render.removeClass(this.Dlogin.nativeElement, 'is-invalid');
+      }
     }
+  }    
+  }
+  checkEmailUserSearch(){
+    if(this.email && this.email.length > 0){
+
+      this.verificationEmail = false;
+      //request server
+      if(this.email.indexOf('@') != -1 && this.email.indexOf('.') != -1){
+        this.statusSearchUserEmail = true;
+        this.statusAlertUserEmail = 0;
+        
+        setTimeout(()=>{
+          
+          if(this.statusSearchUserEmail){
+          this.statusSearchUserEmail = false;
+          console.log('get request Email');
+          this.Suser.checkEmailUser(this.email).subscribe((res)=>{
+            if(res.msj){
+              this.verificationEmail = false;
+              this.statusAlertUserEmail = 2;
+              this.render.removeClass(this.Demail.nativeElement, 'is-valid');
+              this.render.addClass(this.Demail.nativeElement, 'is-invalid');
+            }
+            
+          },(err)=>{
+            if(err.status == 400){
+            if(err.error.msj == false){
+              this.verificationEmail = true;
+              this.statusAlertUserLogin = 0;  
+              this.render.removeClass(this.Demail.nativeElement, 'is-invalid');
+              this.render.addClass(this.Demail.nativeElement, 'is-valid');                
+            }            
+          }
+        });
+      }
+
+    }, 1000);
+  
+    }else{
+      
+      if(this.email.length > 6){
+        this.statusAlertUserEmail = 1;
+      }else{
+        this.statusAlertUserEmail = 0;
+        this.render.removeClass(this.Demail.nativeElement, 'is-valid');
+        this.render.removeClass(this.Demail.nativeElement, 'is-invalid');
+      }
+      
+    }
+  }
   }
 }
--- frontEnd/src/app/service/user.service.ts
+++ frontEnd/src/app/service/user.service.ts
@@ -11,10 +11,13 @@ export class UserService {
     private _http: HttpClient
   ) { }
 
-  registerUser(date: object){
-    //console.log(date);
+  registerUser(date: object): Observable<any>{
+    return this._http.post("/api/user", date);
   }
   checkLoginUser(login: string): Observable<any>{    
     return this._http.get("/api/user/login/"+login);
+  }
+  checkEmailUser(email: string): Observable<any>{
+    return this._http.get("/api/user/email/"+email);
   }
 }
